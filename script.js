ea80abd22362f96b0c9de73f7821470a011d4b6f
Tom <thomas.f.ward.89@gmail.com>
2023-01-20 19:39:42 +0000

    starter code added and html lists rows and columns added

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".saveBtn").on("click",function(){
+
+    
+    //this gets nearby values of description class 
+    var value = $(this).siblings(".description").val();
+    //this is = id of the parent attribute hour-# (e.g.9)
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time,value);
+});
+});
+
+//current date for header 
+var dayWeek = today.format("[Today is] dddd")
+$("#currentDay").text(dayWeek);
+
