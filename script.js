d40bfcf520da77508b10a3ab0e809c8a73cb56bc
Tom <thomas.f.ward.89@gmail.com>
2023-02-11 16:34:32 +0000

    local storage and retreival complete

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-//local storage of text input
 $(document).ready(function () {
+  //on button click, saves to local storage
   $(".saveBtn").on("click", function () {
-    //this gets nearby values of description class
     var value = $(this).siblings(".description").val();
-    //this is = id of the parent attribute hour-# (e.g.9)
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
+  });
+
+  //gets local storage value
+  $(".description").each(function () {
+    var time = $(this).parent().attr("id");
+    var storedValue = localStorage.getItem(time);
+    $(this).val(storedValue);
   });
 });
 
@@ -13,25 +18,18 @@ $(document).ready(function () {
 var today = moment();
 $("#currentDay").text(today.format("D MMM YYYY"));
 
-
-
-
 //check the id #hour, format into moment time
-var timeNow = parseInt((moment().format('HH')));
-console.log(timeNow)
-
-
-
+var timeNow = parseInt(moment().format("HH"));
+console.log(timeNow);
 
 // CSS styling for time change
-
 let i;
 if (timeNow >= 8 && timeNow <= 17) {
-for (i = 0; i < timeNow - 8; i++) {
-$('.time-block').eq(i).css('background-color','grey');
-}
-$('.time-block').eq(timeNow - 9).css('background-color','red ')
+  for (i = 0; i < timeNow - 8; i++) {
+    $(".time-block").eq(i).css("background-color", "grey");
+  }
+  $(".time-block").eq(timeNow - 9).css("background-color", "red ");
 }
 for (let j = i; j < 17; j++) {
-$('.time-block').eq(j).css('background-color','green');
+  $(".time-block").eq(j).css("background-color", "green");
 }
